f0adc4a43e675ee7b13fd2fe4e7879230e03db96
viannaong <viannaong1226@gmail.com>
2026-08-17 02:16:02 +0800

    Validation logic for chess moves

---
+++ moveValidation.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "chessBoardList.h"
+#include "gameLogic.h"
+#include <vector>
+#include <array>
+
+// to use the functions in moveValidation, include "moveValidation.h"
+
+bool isWithinBounds(int startCell[2], int endCell[2]);
+bool isPathClear(const boardArray_t& board, int startCell[2], int endCell[2]);
+bool validateKnight(int startCell[2], int endCell[2]);
+bool validateRook(const boardArray_t& board, int startCell[2], int endCell[2]);
+bool validateBishop(const boardArray_t& board, int startCell[2], int endCell[2]);
+bool validateKing(int startCell[2], int endCell[2]);
+bool validatePawn(const boardArray_t& board, const gameState& state, int startCell[2], int endCell[2]);
+bool isPawnPromotion(const boardArray_t& board, const gameState& state, int startCell[2], int endCell[2]);
+bool validateQueen(const boardArray_t& board, int startCell[2], int endCell[2]);
+bool doesCellHaveMoves(const boardArray_t& board, const gameState& state, int startCell[2], int endCell[2]);
+bool isMoveLegal(const boardArray_t& board, const gameState& state, int startCell[2], int endCell[2]);
+std::vector <std::array<int, 2>>
+getPossibleMoves(const boardArray_t& board, const gameState& state, int startCell[2]);

1bbc4e4d1132c6071d645f6f3bdc2980e747851a
viannaong <viannaong1226@gmail.com>
2026-08-20 02:35:58 +0800

    MAINN.cpp
    -included header files to replace lengthy codes
    -replaced escape codes with chess symbols
    -completed continueGame()
    -undo function pending completion

--- MAINN.cpp
+++ MAINN.cpp
@@ -1,47 +1,18 @@
 #include <iostream>
 #include <iomanip>
+#include <Windows.h>
 #include <array>
 #include <string>
 #include <cstdlib>
 #include <cctype>
+#include "gameLogic.h"
+#include "storage.h"
 
 using namespace std;
 
 // Constants
 const int BOARD_SIZE = 8;
 
-// One board square
-struct Cell
-{
-    string pieceName = "None";
-    string pieceSymbol = "  ";
-    string cellName = "";
-    char cellOwner = 'n';      // w = White, b = Black, n = None
-    bool isEmpty = true;
-    bool hasMoves = false;
-};
-
-// 8x8 board type
-typedef array<array<Cell, BOARD_SIZE>, BOARD_SIZE> boardArray_t;
-
-// Full game state
-struct gameState
-{
-    boardArray_t board;
-
-    bool isWhiteTurn = true;
-
-    // "00" = no en passant target square yet
-    string enPassantTarget = "00";
-
-    bool whiteCanCastleKS = true;
-    bool whiteCanCastleQS = true;
-    bool blackCanCastleKS = true;
-    bool blackCanCastleQS = true;
-
-    string lastMoveText = "Game started";
-    int moveCount = 1;
-};
 
 // The one and only game state currently in play.
 // The board is accessed as currentState.board[col][row].
@@ -58,11 +29,6 @@ int cursorCol = 0;
 int selectedRow = -1;
 int selectedCol = -1;
 
-string columnLetter = "abcdefgh";
-
-// Path to the game state / save file.
-// gameState from this file is the teammate's storage.cpp job.
-string saveFileName = "savegame.txt";
 
 // Clears the console
 void clearScreen()
@@ -162,76 +128,10 @@ void inputPlayerNames()
 
     cout<<"\nBlack Player Name : ";
     getline(cin,blackPlayer);
+
+    createFile(whitePlayer, blackPlayer);
 }
 
-// Builds a fresh starting board
-boardArray_t getNewBoard()
-{
-    boardArray_t temp;
-
-    string pieceName[8] =
-    {
-        "Rook","Knight","Bishop","Queen",
-        "King","Bishop","Knight","Rook"
-    };
-
-    string whitePiece[8] =
-    {
-        "\u2656 ","\u2658 ","\u2657 ","\u2655 ",
-        "\u2654 ","\u2657 ","\u2658 ","\u2656 "
-    };
-
-    string blackPiece[8] =
-    {
-        "\u265C ","\u265E ","\u265D ","\u265B ",
-        "\u265A ","\u265D ","\u265E ","\u265C "
-    };
-
-    // Empty every cell first
-    for(int r=0;r<8;r++)
-    {
-        for(int c=0;c<8;c++)
-        {
-            temp[c][r].cellName=
-            columnLetter[c]+to_string(r+1);
-
-            temp[c][r].pieceName="None";
-            temp[c][r].pieceSymbol="  ";
-            temp[c][r].cellOwner='n';
-            temp[c][r].isEmpty=true;
-        }
-    }
-
-    // White back rank + pawns
-    for(int i=0;i<8;i++)
-    {
-        temp[i][0].pieceName=pieceName[i];
-        temp[i][0].pieceSymbol=whitePiece[i];
-        temp[i][0].cellOwner='w';
-        temp[i][0].isEmpty=false;
-
-        temp[i][1].pieceName="Pawn";
-        temp[i][1].pieceSymbol="\u2659 ";
-        temp[i][1].cellOwner='w';
-        temp[i][1].isEmpty=false;
-    }
-
-    // Black back rank + pawns
-    for(int i=0;i<8;i++)
-    {
-        temp[i][7].pieceName=pieceName[i];
-        temp[i][7].pieceSymbol=blackPiece[i];
-        temp[i][7].cellOwner='b';
-        temp[i][7].isEmpty=false;
-
-        temp[i][6].pieceName="Pawn";
-        temp[i][6].pieceSymbol="\u265F ";
-        temp[i][6].cellOwner='b';
-        temp[i][6].isEmpty=false;
-    }
-
-    return temp;
-}
 
 // Resets game state (board + turn info) for a new game
 void resetGame()
@@ -248,19 +148,7 @@ void resetGame()
     selectedCol=-1;
 }
 
-// Continue-from-menu placeholder
-void continueGame()
-{
-    clearScreen();
 
-    cout<<"==============================\n";
-    cout<<" Continue Game\n";
-    cout<<"==============================\n\n";
-
-    cout<<"Waiting for teammate.\n";
-
-    pauseScreen();
-}
 
 // Exit screen
 void exitScreen()
@@ -477,7 +365,7 @@ void saveGame()
 {
     messageBox(
         "Save Game",
-        "Waiting for teammate implementation."
+        "The Game is Saved"
     );
 }
 
@@ -587,7 +475,7 @@ void gameLoop()
                 break;
 
             case 'U':
-                undoMove();
+                loadLatestGameState ();
                 break;
 
             case 'H':
@@ -604,6 +492,30 @@ void gameLoop()
     }
 }
 
+// Continue-from-menu placeholder
+void continueGame()
+{
+    clearScreen();
+    gameState state = gameState();
+    cout << "==============================\n";
+    cout << " Continue Game\n";
+    cout << "==============================\n\n";
+
+    int maximum = showAllFile();
+
+    cout << "Choose a previous game to resume./n";
+    cout << "(e.g. 1, 2, 3)";
+
+    int choice = getMenuChoice(1, maximum);
+    string fileResume = readFileName(choice);
+    if (accessLastState(fileResume, currentState))
+        gameLoop();
+    else {
+        cout << "Error: Failed to load saved game";
+        pauseScreen();
+    }
+}
+
 // Sets up and runs a new game
 void startNewGame()
 {
@@ -616,13 +528,18 @@ void startNewGame()
 
 int main(int argc, char* argv[])
 {
+    // Enable UTF-8 mode in Windows Console for chess symbols
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     int choice;
 
     // If a game state file was passed on the command line, use it
     // instead of the default. e.g.  ./chess mysave.txt
     if(argc > 1)
     {
-        saveFileName = argv[1];
+        existingFileName = argv[1];
+        // pending completion
     }
 
     showWelcomeScreen();

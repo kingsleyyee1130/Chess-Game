971421c722d7b6231049930daaa58a8810d68840
viannaong <viannaong1226@gmail.com>
2026-08-20 02:38:29 +0800

    chessBoardList.cpp: replaced original boardArray_t with ej's board from MAINN.cpp

--- chessBoardList.cpp
+++ chessBoardList.cpp
@@ -26,35 +26,71 @@ typedef array<array<Cell, 8>, 8> boardArray_t; //to replace lengthy 8x8 array ty
 
 boardArray_t getNewBoard() {
     boardArray_t board;
-    int row = 8;
-    int col = 8;
+    string pieceName[8] =
+    { 
+        "Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"
+    };
 
-    for (int y=0; y < row; y++) { //setting cell names (eg. a1, e4, h8 ...)
-        int rank = y + 1;
-        for (int x=0; x < col; x++) {
-            board[x][y].cellName = col_alphabet[x] + to_string(rank);
+    string whitePiece[8] =
+    {
+        "♜ ", "♞ ", "♝ ", "♛ ", "♚ ", "♝ ", "♞ ", "♜ "
+    };
+
+    string blackPiece[8] =
+    {
+        "♖ ", "♘ ", "♗ ", "♕ ", "♔ ", "♗ ", "♘ ", "♖ "
+    };
+
+    // Empty every cell first
+    for (int r = 0;r < 8;r++)
+    {
+        for (int c = 0;c < 8;c++)
+        {
+            string columnLetter = "abcdefgh";
+            board[c][r].cellName =
+                columnLetter[c] + to_string(r + 1);
+
+            board[c][r].pieceName = "None";
+            board[c][r].pieceSymbol = "  ";
+            board[c][r].cellOwner = 'n';
+            board[c][r].isEmpty = true;
         }
     }
-    for (int x=0; x < col; x++) { //setting statuses column by column
-        for (int i : {0,1}) { //setting white chess cell status
-            board[x][i].pieceName   = (i==0 ? chess_names[x] : chess_names[8]);
-            board[x][i].pieceSymbol = (i==0 ? chess_symbol_white[x] : chess_symbol_white[8]);
-            board[x][i].cellOwner   = 'w';
-            board[x][i].isEmpty     = false;
-        }
-        for (int i : {6,7}) { //setting black chess cell status
-            board[x][i].pieceName   = (i==7 ? chess_names[x] : chess_names[8]);
-            board[x][i].pieceSymbol = (i==7 ? chess_symbol_black[x] : chess_symbol_black[8]);
-            board[x][i].cellOwner   = 'b';
-            board[x][i].isEmpty     = false;
-        }
+
+    // White back rank + pawns
+    for (int i = 0;i < 8;i++)
+    {
+        board[i][0].pieceName = pieceName[i];
+        board[i][0].pieceSymbol = whitePiece[i];
+        board[i][0].cellOwner = 'w';
+        board[i][0].isEmpty = false;
+
+        board[i][1].pieceName = "Pawn";
+        board[i][1].pieceSymbol = "♟\xEF\xB8\x8E ";
+        board[i][1].cellOwner = 'w';
+        board[i][1].isEmpty = false;
     }
+
+    // Black back rank + pawns
+    for (int i = 0;i < 8;i++)
+    {
+        board[i][7].pieceName = pieceName[i];
+        board[i][7].pieceSymbol = blackPiece[i];
+        board[i][7].cellOwner = 'b';
+        board[i][7].isEmpty = false;
+
+        board[i][6].pieceName = "Pawn";
+        board[i][6].pieceSymbol = "♙ ";
+        board[i][6].cellOwner = 'b';
+        board[i][6].isEmpty = false;
+    }
+
     return board;
 }
 
 
 void printChessArrangement(boardArray_t board) {
-    SetConsoleOutputCP(CP_UTF8);
+    
     for (int y=7; y >=0 ; y--) {
         for (int x=0; x < 8; x++) {
             cout << board[x][y].pieceSymbol << "  " ;
